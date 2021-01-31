aca4802241f18572b650248d2ffdf292f06c7042
Makusu-dev <maxime.jeannin@hotmail.fr>
2021-01-31 03:04:34 +0100

    Ajout des sprites plateformes et du Prefab Plateforme

---
+++ Assets/Script/PlatformTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTile : MonoBehaviour
+{
+    [SerializeField] int tileNumber;
+    [SerializeField] Sprite[] tiles;
+    
+        
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = tiles[tileNumber];
+    }
+
+    // Update is called once per frame
+    
+}
