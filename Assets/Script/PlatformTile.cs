56c6c8b089ab9acffb1952bb8dec49026720ab48
Taigo <TaigoFR92@gmail.com>
2021-01-31 09:41:18 +0100

    Automatisation of tile

--- Assets/Script/PlatformTile.cs
+++ Assets/Script/PlatformTile.cs
@@ -16,14 +16,15 @@ public class PlatformTile : MonoBehaviour
     public bool up;
     public bool down;
 
+    private Collision coll;
+
     private int testTile;
 
     [Header("Collision")]
 
     public float collisionRadius = 0.25f;
     public Vector2 upOffset, bottomOffset, rightOffset, leftOffset;
-    private Color debugCollisionColor = Color.red;
-
+    private Color debugCollisionColor = Color.red;    
 
     // Start is called before the first frame update
     void Awake()
@@ -95,78 +96,7 @@ public class PlatformTile : MonoBehaviour
         gameObject.GetComponent<SpriteRenderer>().sprite = tiles[testTile];
     }
 
-    // Update is called once per frame
-
-    // Update is called once per frame
-    void Update()
-    {
-        down = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
-
-        right = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
-            
-        left = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
-
-        up = Physics2D.OverlapCircle((Vector2)transform.position + upOffset, collisionRadius, groundLayer);
-
-
-        if (!up)
-        {
-            if (!left)
-            {
-                Debug.Log("0");
-                testTile = 0;
-            }
-            else if (!right)
-            {
-                Debug.Log("2");
-                testTile = 2;
-            }
-            else
-            {
-                Debug.Log("1");
-                testTile = 1;
-            }
-        }
-        else if (!down)
-        {
-            if (!left)
-            {
-                Debug.Log("6");
-                testTile = 6;
-            }
-            else if (!right)
-            {
-                Debug.Log("8");
-                testTile = 8;
-            }
-            else
-            {
-                Debug.Log("7");
-                testTile = 7;
-            }
-        }
-        else
-        {
-            if (!left)
-            {
-                Debug.Log("3");
-                testTile = 3;
-            }
-            else if (!right)
-            {
-                Debug.Log("5");
-                testTile = 5;
-            }
-            else
-            {
-                Debug.Log("4");
-                testTile = 4;
-            }
-        }
-
-
-        gameObject.GetComponent<SpriteRenderer>().sprite = tiles[testTile];
-    }
+    
 
     void OnDrawGizmos()
     {
@@ -180,5 +110,5 @@ public class PlatformTile : MonoBehaviour
         Gizmos.DrawWireSphere((Vector2)transform.position + upOffset, collisionRadius);
 
     }
-
+    
 }
