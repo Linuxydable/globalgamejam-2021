9f61c86ae99a168f41a0c870679a7cf2a49e332d
Makusu-dev <maxime.jeannin@hotmail.fr>
2021-01-31 11:11:03 +0100

    Création du prefab Sol

---
+++ Assets/Script/Sol_Mur.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sol_Mur : MonoBehaviour
+{
+    [SerializeField] int colonnes;
+    [SerializeField] int lignes;
+    [SerializeField] PlatformTile tile;
+
+    float offset = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int j = 0; j < lignes; j++)
+        {
+            for (int i = 0; i < colonnes; i++)
+            {
+                //if (i == 0&j==0)
+                //{
+                //   Instantiate(tile, gameObject.transform);
+                //}
+
+                Vector2 newPos = new Vector2(gameObject.transform.position.x + i * offset, gameObject.transform.position.y + j * (-offset));
+                PlatformTile newPlatformTile = Instantiate(tile,newPos,transform.rotation);
+                newPlatformTile.transform.SetParent(gameObject.transform);
+            }
+
+        }
+        
+    }
+
+    // Update is called once per frame
+}
